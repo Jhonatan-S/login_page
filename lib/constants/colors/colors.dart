93cdef7d94924d8819255ca6df18e5baa37663e9
Jhonatan-S <studyprogrammer5@gmail.com>
2023-07-06 21:57:08 -0300

    Atualia App

---
+++ lib/constants/colors/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+
+  final Gradient gradientBtn = const LinearGradient(begin: Alignment.topCenter, end: Alignment.bottomCenter ,colors: [  Color(0xff9C3FE4),  Color(0xffC65647)]);
+
+  final Gradient gradientInput = const LinearGradient(colors: [Color(0xff53495A), Color(0xff281E28)]);
+
+}
---
+++ lib/constants/colors/images.dart
@@ -0,0 +1,8 @@
+
+
+class ImageCustom {
+  ImageCustom._();
+
+  final String signInImg = 'assets/images/img_login.png';
+  final String signUpImage = 'assets/images/img_sign_up.png';
+}
